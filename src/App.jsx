6ac16d3004e1ce4094557f668ea9e0f5e9ce4b68
Pratikesh Kumar <122167622+Pratikesh-kumar@users.noreply.github.com>
2023-02-20 18:41:33 +0000

    app change

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,11 @@
 import React from 'react';
-import './App.css';
-import Person from './components/Person';
-import PersonData from './components/PersonData';
-
-// const PersonInfo = (value) => {
-//   return (
-//   <>
-  // <Person 
-    // title={value.title}
-      // des={value.des}
-      // />
-  // </>
-//   )
-
-// }
 
 export default function App() {
 
   return (
     <>
-      <h1>Straw Hats Pirates  </h1>
-      
-      <div className='container'>
-        
-        {PersonData.map((value)=>{
-         return (
-           <>
-            <Person
-              title={value.title}
-              img={value.img}
-                des= {value.des}
-              />
-             </>
-           )
-        })}
+      <h1>  new theme  </h1>
 
-      </div>
     </>
   )
 }
